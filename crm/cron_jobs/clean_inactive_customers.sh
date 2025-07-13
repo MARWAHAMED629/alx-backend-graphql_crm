a829f1f189660640853e735dbf3561b448201206
MARWAHAMED629 <mmero385@gmail.com>
2025-07-13 03:54:57 +0200

    clean_inactive_customers.sh

--- crm/cron_jobs/clean_inactive_customers.sh
+++ crm/cron_jobs/clean_inactive_customers.sh
@@ -3,7 +3,9 @@
 
 cd "$(dirname "${BASH_SOURCE[0]}")/../.."
 
-if [ -d "$(pwd)" ]; then
+cwd=$(pwd)
+
+if [ -d "$cwd" ]; then
     COUNT=$(python manage.py shell -c "from crm.models import Customer, Order; from django.utils import timezone; from datetime import timedelta; one_year_ago = timezone.now() - timedelta(days=365); to_delete = Customer.objects.exclude(order__created_at__gte=one_year_ago); deleted, _ = to_delete.delete(); print(deleted)")
     echo "$(date '+%Y-%m-%d %H:%M:%S') - Deleted $COUNT inactive customers" >> /tmp/customer_cleanup_log.txt
 else
